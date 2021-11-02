d965db581efe23c51869f56af9e07cccc1c3d4ed
NghiaSy <nghiasy98vp@gmail.com>
2021-11-02 17:19:43 +0700

    up

---
+++ JS/Login.js
@@ -0,0 +1,16 @@
+const DEFAULT_USERNAME = "admin";
+const DEFAULT_PASSWORD = "123456";
+function validateFormLogin() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (username !== DEFAULT_USERNAME || password !== DEFAULT_PASSWORD){
+        alert("Đăng nhập thất bại, mời đăng nhập lại!");
+        return false;
+    }else if (username === DEFAULT_USERNAME && password === DEFAULT_PASSWORD){
+        alert("Đăng nhập thành công!");
+       
+    }
+}
+function troll() {
+        alert("Cái này để cho đẹp chứ không có tác dụng gì");
+}
